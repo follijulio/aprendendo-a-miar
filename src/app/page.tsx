188506eaebe4aee0126b38edf10a19692de34c24
Júlio César <114767812+NETUNO14@users.noreply.github.com>
2023-06-11 00:56:32 -0300

    implemetation of perfil card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Perfil from "@/components/Perfil";
+import Perfil from "@/components/PerfilCard";
+import { perfil } from "@/mock/perfil";
+import { link } from "fs";
 import Image from "next/image";
 import Link from "next/link";
 export default function Home() {
   return (
-    <div className="h-screen bg-red-800 justify-center place-items-center">
+    <div className="h-screen bg-[#0b062d]">
+      <div className="p-20">
+        <Perfil
+          nickName={perfil.nickName}
+          nome={perfil.nome}
+          imageLink={perfil.foto}
+          idade={perfil.idade}
+          bio={perfil.bio}
+          link={perfil.link}
+        />
+      </div>
     </div>
   );
 }
+/*
+ */
