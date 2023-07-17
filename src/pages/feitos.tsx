edb983268a1bdfeecd616a9038ed86b24af99e49
Júlio César <114767812+NETUNO14@users.noreply.github.com>
2023-07-16 23:09:40 -0300

    criação da page "feitos" para utilizações futuras

---
+++ src/pages/feitos.tsx
@@ -0,0 +1,13 @@
+import BoxText from "@/components/CardButton";
+import CardText from "@/components/CardText";
+import LayoutPrincipal from "@/layouts/PrincipalLayout";
+import Link from "next/link";
+
+const feitos: React.FC = () => {
+  return (
+    <LayoutPrincipal >
+      <CardText/>
+    </LayoutPrincipal>
+  );
+};
+export default feitos;
