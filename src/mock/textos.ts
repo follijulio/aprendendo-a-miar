235dfcfc8412b1f882242e1eae25ecada8c99226
folli14 <cesarfolli@gmail.com>
2023-12-09 10:31:22 -0300

    atualização do mock

--- src/mock/textos.ts
+++ src/mock/textos.ts
@@ -2,23 +2,19 @@ const textos = [
   {
     titulo: "Onde estudo?",
     text: "Estudante de TI no Instituto Federal de Alagoas",
-    eLink: false
   },
   {
     titulo: "O que estudo?",
     text: "Dev Junior, com experiência em front-end",
-    eLink: false
-  },
-  {
-    titulo: "Qual minhas prefências?",
-    text: "Linguagens com que já trabalhei: HTML, CSS, JS, TAILWIND CSS, REACT JS, TYPESCRIPT, JAVA, C, PYTHON",
-    eLink: true,
-    link: "/estudos"
   },
   {
     titulo: "miau",
     text: "ATÉ O MOMENTO, ESSE SITE ESTÁ EM PROGRESSO, OU SEJA, AINDA ESTÁ SENDO FEITO",
-    eLink: false,
+  },
+  {
+    text: "Linguagens com que já trabalhei: HTML, CSS, JS, TAILWIND CSS, REACT JS, TYPESCRIPT, JAVA, C, PYTHON",
+    key: "Qual minhas prefências?",
+    link: "/estudos",
   },
 ];
 export { textos };
