7e5ed5c57872b94d30937e46179ed535f9d8a156
folli14 <cesarfolli@gmail.com>
2023-12-09 10:28:52 -0300

    home com atualizações

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,18 +4,17 @@ import { textos } from '@/mock/textos';
 
 const Home: React.FC = () => {
     return (
-        <LayoutHome durationNav={2100}>
+        <LayoutHome durationNav={2100} NavBarActive={true}>
             <div>
                 <section id="cards">
                     <div className="gap-y-6 gap-x-6 flex flex-wrap justify-evenly">
                         {textos.map((text) => {
                             return (
-                                <CardText key={text.titulo} text={text.text} link={text.link} eLink={false} />
+                                <CardText key={text.titulo} text={text.text} link={text.link} />
                             )
                         })}
                     </div>
                 </section>
-
             </div>
         </LayoutHome>
     );
