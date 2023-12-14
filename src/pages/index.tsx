98660930d4d0b556ffbf91c502f7b3dfa0216114
folli14 <cesarfolli@gmail.com>
2023-12-14 11:10:45 -0300

    removendo comentários

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,13 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function Index() {
-  //* hooks
+
   const router = useRouter();
 
-  //* lifecycle
   useEffect(() => {
     router.replace("/home");
   }, [router]);
 
-  //* render
+
   return;
 }
