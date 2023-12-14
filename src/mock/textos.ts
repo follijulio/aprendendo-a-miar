39d5e7db487bcbb79a4cdcb2b5fad7ef34743d82
folli14 <cesarfolli@gmail.com>
2023-12-14 11:15:40 -0300

    alteração da ordem

--- src/mock/textos.ts
+++ src/mock/textos.ts
@@ -8,13 +8,15 @@ const textos = [
     text: "Dev Junior, com experiência em front-end",
   },
   {
-    titulo: "miau",
-    text: "ATÉ O MOMENTO, ESSE SITE ESTÁ EM PROGRESSO, OU SEJA, AINDA ESTÁ SENDO FEITO",
-  },
-  {
     text: "Linguagens com que já trabalhei: HTML, CSS, JS, TAILWIND CSS, REACT JS, TYPESCRIPT, JAVA, C, PYTHON",
     key: "Qual minhas prefências?",
     link: "/estudos",
   },
+  {
+    titulo: "miau",
+    text: "ATÉ O MOMENTO, ESSE SITE ESTÁ EM PROGRESSO, OU SEJA, AINDA ESTÁ SENDO FEITO",
+  },
+  
+  
 ];
 export { textos };
