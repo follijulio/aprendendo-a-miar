3ff1bec0d3e14857207e14c9d52418c0eafec3ae
folli14 <cesarfolli@gmail.com>
2023-12-16 12:09:07 -0300

    implementação dos cards a page "sobre"

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -1,3 +1,5 @@
+import Card from "@/components/Card";
+import CardImage from "@/components/CardImage";
 import { Maquina } from "@/components/Maquina";
 import NavBar from "@/components/NavBar";
 import LayoutPrincipal from "@/layouts/PrincipalLayout";
@@ -10,27 +12,62 @@ const Sobre: React.FC = () => {
     useEffect(() => {
         const duration = 2000;
         const cont = setTimeout(() => {
-            setTime(false); 
+            setTime(false);
         }, duration);
         return () => clearTimeout(cont);
     }, []);
 
+    const [timeTwo, setTimeTwo] = useState(true);
+    useEffect(() => {
+        const duration = 2750;
+        const cont = setTimeout(() => {
+            setTimeTwo(false);
+        }, duration);
+        return () => clearTimeout(cont);
+
+
+    }, []);
     return (
         <LayoutPrincipal>
             {time ? <div></div> : <NavBar />}
             <div className="w-full flex justify-center">
                 <div className="flex flex-col items-center">
-                    <img
-                        src={netuno.fotoRosto}
-                        className={time ? 'h-52' : 'py-4 rounded-full transition-all duration-1000 h-32'}
-                        id="foto-arredondada"
-                        alt={"netuno"}
-                    />
+                    {timeTwo ?
+                        <img
+                            src={netuno.fotoRosto}
+                            className={time ? 'h-52 opacity-100' : 'py-4 rounded-full transition-all duration-1000 h-32 opacity-0'}
+                            id="foto-arredondada"
+                            alt={"netuno"}
+                        />
+                        :
+                        <>
+                        </>
+                    }
                     <div className="text-6xl">
                         <Maquina text="Este é o Júlio" escrever={false} />
                     </div>
-                    <div className="text-9xl">
-                        FUI ESCOVAR OS DENTES
+                    {/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/}
+                    <div className={timeTwo ? 'opacity-0' : 'transition-opacity duration-1000 opacity-100'}>
+                        <div className="flex justify-center items-center h-full">
+                            <div className="flex flex-col">
+                                <div className="flex">
+                                    <CardImage rosto={true} imageLink="https://github.com/follijulio.png" texto="Olá! Sou Júlio César, um apaixonado por tecnologia e programação nascido em Alagoas em 2006. Desde cedo, meu fascínio por códigos e sites me levou a explorar o vasto mundo da programação." />
+                                    <div>
+                                        <Card texto="Atualmente, estudo no Instituto Federal de Alagoas (IFAL), onde curso Informática. Iniciei minha jornada aos 14 anos, inspirado por vídeos online que despertaram minha paixão pela criação de sites e códigos. Dedico cerca de 8 horas diárias ao estudo e aprimoramento nessa área fascinante." />
+                                        <Card texto="Dedico meu tempo ao estudo e ao desenvolvimento constante. Com a bagagem do IFAL, busco explorar novos horizontes na área da informática. Cada linha de código é uma oportunidade de aprendizado, e estou ansioso para contribuir ainda mais para a comunidade tecnológica no futuro." />
+                                    </div>
+                                </div>
+                                <div className="flex">
+                                    <div>
+                                        <Card texto="Meus valores são simples, mas fundamentais para minha jornada. Acredito na honestidade, perseverança, alegria e calma como pilares essenciais para alcançar meus objetivos. Esses princípios orientam minha conduta tanto na vida quanto na programação." />
+                                        <a href="https://www2.ifal.edu.br">
+                                        <Card texto="INSTUTUTO FEDERAL DE ALAGOAS"/>
+                                        </a>
+                                    </div>
+                                    <CardImage imageLink="https://www.amsterdam.net/en/wp-content/uploads/sites/136/johan-cruijff-arena-hd.jpg" texto="Meus sonhos vão além do código. Um deles é viajar para os Países Baixos e visitar a Johan Cruyff Arena, o estádio do AJAX. Cruyff, meu maior ídolo, deixou um legado inspirador no futebol, e esse estádio representa um marco significativo em sua carreira." />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -39,92 +76,3 @@ const Sobre: React.FC = () => {
 };
 
 export default Sobre;
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-<div className="flex justify-center p-6">
-                <img
-                    src={netuno.fotoRosto}
-                    className={time ? 'h-52' : 'rounded-full transition-all duration-1000 h-32'}
-                    id="foto-arredondada"
-                    alt={"netuno"}
-                />
-            </div>
-            <div>
-                <div className="flex justify-center">
-                    {time ? (
-                        <div className="font-bold text-4xl text-white">
-                            {timeThree ? <span>BEM VINDO</span> : <Maquina text="BEM VINDO" escrever={false} />}
-
-                        </div>
-                    ) : (
-                        <div className='font-bold text-4xl text-white miau'>
-                            <Maquina text="MEU NOME É NETUNO" escrever={true} />
-                        </div>
-                    )}
-                </div>
-
-                <div className="flex justify-center p-6">
-                <img
-                    src={netuno.fotoRosto}
-                    className={time ? 'h-52' : 'rounded-full transition-all duration-1000 h-32'}
-                    id="foto-arredondada"
-                    alt={"netuno"}
-                />
-            </div>
-            <div>
-                <div className="flex justify-center">
-                    {time ? (
-                        <div className="font-bold text-4xl text-white">
-                            {timeThree ? <span>BEM VINDO</span> : <Maquina text="BEM VINDO" escrever={false} />}
-
-                        </div>
-                    ) : (
-                        <div className='font-bold text-4xl text-white miau'>
-                            <Maquina text="MEU NOME É NETUNO" escrever={true} />
-                        </div>
-                    )}
-                </div>
-<div>
-                        <img
-                            src={netuno.fotoRosto}
-                            className={'rounded-full transition-all duration-1000 h-28'}
-                            id="foto-arredondada"
-                            alt={"netuno"}
-                        />
-                    </div>
-                    <div>
-                        <div className="text-6xl">
-                        <Maquina text="MEU NOME É NETUNO" escrever={false}/>
-                        </div>
-                    </div>
-
-<div className="flex items-center">
-                    <div>
-                        <img
-                            src={netuno.fotoRosto}
-                            className={'rounded-full transition-all duration-1000 h-28'}
-                            id="foto-arredondada"
-                            alt={"netuno"}
-                        />
-                    </div>
-                    <div>
-                        s
-                    </div>
-                </div>
-
-
-
-
-*/
