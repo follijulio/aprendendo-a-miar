3ac4eb61e71fdb4687b86325dbe995ec282ef065
folli14 <cesarfolli@gmail.com>
2023-12-18 01:40:03 -0300

    responsividade em card image

--- src/components/CardImage.tsx
+++ src/components/CardImage.tsx
@@ -9,15 +9,19 @@ const CardImage: React.FC<CardProps> = ({ imageLink, texto, rosto }) => {
     return (
         <div className="border-2 border-sky-500 shadow-md shadow-sky-500 rounded-2xl bg-transparent m-8">
             <div className="h-80 flex justify-center items-center">
-                <div className="flex p-4">
-                    {rosto ?
-                        <img className="h-48 w-56 object-cover rounded-lg image-move-efect" src={imageLink} alt={"miau"} />
-                        :
-                        <img className="h-48 w-56 object-cover rounded-lg" src={imageLink} alt={"miau"} />
-                    }
-                    <p className="text-white text-lg font-semibold indent-10 p-4 text-justify no-scrollbar overflow-y-auto h-full">
-                        {texto}
-                    </p>
+                <div className="md:flex flex flex-wrap">
+                    <div className="p-4 md:w-1/2 flex justify-center items-center w-full">
+                        <img
+                            className="h-28 w-48 md:h-48 md:w-56 object-cover rounded-lg"
+                            src={imageLink}
+                            alt={"miau"}
+                        />
+                    </div>
+                    <div className="md:w-1/2">
+                        <p className="text-white text-lg font-semibold indent-10 p-4 text-justify no-scrollbar overflow-y-auto md:h-full h-40">
+                            {texto}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
