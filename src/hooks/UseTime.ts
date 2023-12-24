46b9310abb28827984a9ccfe41256cc5e8a0db17
folli14 <cesarfolli@gmail.com>
2023-12-24 14:45:46 -0300

    criação de hook

---
+++ src/hooks/UseTime.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+interface UseTimeoutOptions {
+  duration: number;
+  onTimeout?: () => void;
+}
+
+const useTime = ({ duration, onTimeout }: UseTimeoutOptions): [boolean, () => void] => {
+  const [time, setTime] = useState(true);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setTime(false);
+      onTimeout?.();
+    }, duration);
+
+    return () => {
+      clearTimeout(timeoutId);
+      setTime(false);
+    };
+  }, [duration, onTimeout]);
+
+  return [time, () => setTime(true)];
+};
+
+export default useTime;
