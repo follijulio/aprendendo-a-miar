bcc0afe9844f259378fa4928995ea9d106e623c3
folli14 <cesarfolli@gmail.com>
2023-12-30 10:55:01 -0300

    component styling

--- src/components/ContatoCard.tsx
+++ src/components/ContatoCard.tsx
@@ -7,7 +7,7 @@ interface contatoProps {
 }
 const ContatoCard: React.FC<contatoProps> = ({ image, link, titulo }) => {
     return (
-        <Link href={link} className="w-32 h-10 border-sky-400 rounded-md m-2 flex items-center p-2 bg-sky-700 hover:bg-sky-400 duration-500 hover:scale-105">
+        <Link href={link} className="w-32 h-10 border-sky-600 shadow-md shadow-black rounded-md m-1 flex items-center p-2 bg-sky-700 hover:bg-sky-400 duration-300 hover:scale-105 hover:shadow-lg hover:shadow-slate-400">
             <div className="flex items-center w-full justify-between">
                 <img src={image} alt={titulo} className="h-4 w-4" />
                 <span className="text-sm font-bold font-mono">
