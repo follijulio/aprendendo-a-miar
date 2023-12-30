c45289351db046f3c27aa9c7c02e233cdd03a3b3
folli14 <cesarfolli@gmail.com>
2023-12-30 02:37:42 -0300

    criação de componente excencial para a sidebar

--- src/components/ContatoCard.tsx
+++ src/components/ContatoCard.tsx
@@ -1,9 +1,22 @@
+import Link from "next/link";
 
-const ContatoCard: React.FC = () => {
+interface contatoProps {
+    link: string;
+    image: string;
+    titulo: string;
+}
+const ContatoCard: React.FC<contatoProps> = ({ image, link, titulo }) => {
     return (
-        <button>
-
-        </button>
+        <Link href={link} className="w-32 h-10 border-sky-400 rounded-md m-2 flex items-center p-2 bg-sky-700 hover:bg-sky-400 duration-500 hover:scale-105">
+            <div className="flex items-center w-full justify-between">
+                <img src={image} alt={titulo} className="h-4 w-4" />
+                <span className="text-sm font-bold font-mono">
+                    {titulo}
+                </span>
+                <div>
+                </div>
+            </div>
+        </Link>
     )
 }
 export default ContatoCard;
