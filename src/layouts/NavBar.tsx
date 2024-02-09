9754d2e47e7b0276ff0faa5e37a0fc94295361b9
folli14 <cesarfolli@gmail.com>
2024-02-08 22:17:00 -0300

    navbar fixada no topo

--- src/layouts/NavBar.tsx
+++ src/layouts/NavBar.tsx
@@ -40,7 +40,7 @@ const NavBar: React.FC = () => {
     return (
         <div>
             {time ? (<div></div>) : (
-                <header className={timeTwo ? "h-0" : "transition-all duration-700 h-20 w-full flex items-center justify-between p-4 bg-gradient-to-t from-black/40 via-black/90 to-black/100 rounded-b-lg shadow-lg shadow-slate-900"}>
+                <header className={timeTwo ? "h-0" : "fixed top-0 left-0 right-0 bg transition-all duration-700 h-16 w-full flex items-center justify-between p-4 bg-black rounded-b-lg shadow-lg shadow-slate-900"}>
 
 
                     {timeTwo ? <div></div> :
