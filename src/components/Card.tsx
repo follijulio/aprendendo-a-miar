4d0402bccacd9cbac0a4973413ca2613547f3ef6
folli14 <cesarfolli@gmail.com>
2024-02-08 22:06:20 -0300

    remove parte desnecessária de componente

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,27 +6,13 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ texto, link }) => {
     return (
-        <div>
-            {link ?
-                <a href={link}>
-                    <div className="border-2 border-sky-500 shadow-md h-40 w-96 shadow-sky-500 rounded-2xl bg-transparent m-8">
-                        <div className="p-2 indent-2 text-justify">
-                            <p className="text-white">
-                                {texto}
-                            </p>
-                        </div>
-                    </div>
-                </a>
-                :
-                <div className="border-2 border-sky-500 shadow-md h-40 w-96 shadow-sky-500 rounded-2xl bg-transparent m-8">
-                    <div className=" p-2 indent-2 text-justify">
-                        <p className="text-white">
-                            {texto}
-                        </p>
-                    </div>
-                </div>
-            }
-        </div >
+        <div className="border-2 border-sky-500 shadow-md h-40 w-40 shadow-sky-500 rounded-md bg-transparent m-8">
+            <div className=" p-2 indent-2 text-justify">
+                <p className="text-white">
+                    {texto}
+                </p>
+            </div>
+        </div>
     )
 };
 export default Card;
