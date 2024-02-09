0582970b5fd851f934496906253bd2a1dbdc23bd
folli14 <cesarfolli@gmail.com>
2024-02-08 22:09:30 -0300

    resolve a falta de responsividade de imagem no card

--- src/components/CardImage.tsx
+++ src/components/CardImage.tsx
@@ -12,7 +12,7 @@ const CardImage: React.FC<CardProps> = ({ imageLink, texto, rosto }) => {
                 <div className="md:flex flex flex-wrap">
                     <div className="p-4 md:w-1/2 flex justify-center items-center w-full">
                         <img
-                            className={rosto? `w-48 md:w-56 object-cover rounded-lg image-move-efect` : `h-28 w-48 md:h-48 md:w-56 object-cover rounded-lg`}
+                            className={rosto ? `w-40 md:w-56 object-cover rounded-lg image-move-efect` : `h-28 w-48 md:h-48 md:w-56 object-cover rounded-lg`}
                             src={imageLink}
                             alt={"miau"}
                         />
