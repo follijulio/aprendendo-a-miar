abe985740a754dea6db6b1f38acf52482d2a0b9e
folli14 <cesarfolli@gmail.com>
2024-02-10 19:35:22 -0300

    fix card

--- src/components/CardChildren.tsx
+++ src/components/CardChildren.tsx
@@ -5,7 +5,7 @@ interface CardProps {
 
 const CardChildren: React.FC<CardProps> = ({children}) => {
     return (
-        <div className="border-2 border-sky-500 shadow-md shadow-sky-500 rounded-2xl bg-transparent m-8">
+        <div className="border-2 border-sky-900 rounded-md bg-transparent m-8">
             <div className="h-40  p-2 indent-2 text-justify ">
                 <p className="no-scrollbar overflow-y-auto h-full text-justify text-white">
                     {children}
