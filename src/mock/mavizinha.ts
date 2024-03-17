509e8ec296007fac79200d9956bc51cc7d07324c
folli14 <cesarfolli@gmail.com>
2024-03-16 23:13:11 -0300

    mock mavi

--- src/mock/mavizinha.ts
+++ src/mock/mavizinha.ts
@@ -1,5 +1,21 @@
 const mavizinha = [
-  { text: "MAVI"}
+  { text: "Assim como as estrelas ao anoitecer, meus olhos brilham ao te ver" },
+  {
+    text: "Te ver é o momento auge do meu dia, posso até estar triste, mas logo que te vejo, fico feliz",
+  },
+  { text: "Seu sorriso ilumina até os meus dia mais sombrios, trazendo luz e alegria." },
+  { text: "Cada vez que penso em você, meu coração bate mais forte, é como se ele soubesse que você está perto." },
+  { text: "Com você em meus braços, encontro o conforto que me faz sentir em casa, protegido pela sua presença." },
+  
 ];
 
+
 export { mavizinha };
+
+const rota = { rota: "https://youtu.be/iyJDuJggiEM?si=zSufDpZ5vAvyAAVk" };
+
+export { rota };
+
+const hearts = [{ coracao: "❤️" }, { coracao: "❤️" }, { coracao: "❤️" }];
+
+export { hearts };
